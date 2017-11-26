47839394b2b73899c06283167059322d5bc4a826
Renato Martins <renatto.martins@gmail.com>
2017-11-26 16:38:58 -0200

    [@dmgv] Format code

--- src/main/java/asd/devops/aula03/MoneyWriter.java
+++ src/main/java/asd/devops/aula03/MoneyWriter.java
@@ -14,17 +14,16 @@ public class MoneyWriter {
 
 	public static String toNumberInFull(Money money) {
 		String numberInFull = "";
-		double number = money.getValue();  
-		
-		if ( number <= 0 || number % 1 != 0 || number > 999.99)
-		{
+		double number = money.getValue();
+
+		if (number <= 0 || number % 1 != 0 || number > 999.99) {
 			return "Valor inválido";
 		}
 
 		int hundred = (int) number / 100;
 		int ten = (int) ((number % 100) / 10);
 		int unit = (int) number % 10;
-		
+
 		// hundreds
 		if (hundred > 0) {
 			numberInFull = MoneyWriter.HUNDREDS[hundred];
@@ -32,24 +31,24 @@ public class MoneyWriter {
 				numberInFull += MoneyWriter.CONECTOR;
 			}
 		}
-		
+
 		if (number == 100) {
 			numberInFull = MoneyWriter.HUNDRED;
 		}
-		
+
 		// dozens
 		if (ten > 1) {
 			numberInFull += MoneyWriter.DOZENS[ten];
-			if ( unit > 0) {
+			if (unit > 0) {
 				numberInFull += MoneyWriter.CONECTOR;
 			}
 		}
-		
+
 		// teens
 		if (ten == 1) {
 			numberInFull += MoneyWriter.TEENS[unit];
 		}
-		
+
 		// units
 		if (unit > 0 && ten != 1) {
 			numberInFull += MoneyWriter.UNITS[unit];
