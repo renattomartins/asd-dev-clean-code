8749a4ebe8064bc5e4cd03edb46bd81b34aee0ba
Renato Martins <renatto.martins@gmail.com>
2017-11-26 12:22:57 -0200

    [@dmgv] Conclude code that writes 3-digits number in full

--- src/main/java/asd/devops/aula03/Money.java
+++ src/main/java/asd/devops/aula03/Money.java
@@ -1,39 +1,60 @@
 package asd.devops.aula03;
 
-public class Money 
-{
-    private double money;
-    
-    private static final String[] UNITS = {"", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove"};
-    private static final String[] TEENS = {"", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"};
-    private static final String[] DOZENS = {"", "dez", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"};
-    private static final String[] HUNDREDS = {"", "cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"};
-    private static final String HUNDRED = "cem";
-    private static final String CONECTOR = " e ";
-    private static final String CURRENCY_NAME = " reais";
+public class Money {
+	private double money;
+
+	private static final String[] UNITS = { "", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove" };
+	private static final String[] TEENS = { "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove" };
+	private static final String[] DOZENS = { "", "", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa" };
+	private static final String[] HUNDREDS = { "", "cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos" };
+	private static final String HUNDRED = "cem";
+	private static final String CONECTOR = " e ";
+	private static final String CURRENCY_NAME = " reais";
 
 	public Money(double money) {
 		this.money = money;
 	}
-	
+
 	public String toNumberInFull() {
 		String numberInFull = "";
+
+		int hundred = (int) this.money / 100;
+		int ten = (int) ((this.money % 100) / 10);
+		int unit = (int) this.money % 10;
 		
-		int hundred = (int)this.money / 100;
-		int ten = (int)((this.money % 100) / 10);
-		int unit = (int)this.money % 10;
-		
-		if ( hundred > 0 ) {
-			numberInFull = Money.HUNDREDS[hundred] + Money.CONECTOR;
+		// hundreds
+		if (hundred > 0) {
+			numberInFull = Money.HUNDREDS[hundred];
+			if (ten > 0 || unit > 0) {
+				numberInFull += Money.CONECTOR;
+			}
 		}
 		
-		if ( ten > 0 ) {
-			numberInFull += Money.DOZENS[ten] + Money.CONECTOR;
+		if (this.money == 100) {
+			numberInFull = Money.HUNDRED;
 		}
 		
-		numberInFull += Money.UNITS[unit] + Money.CURRENCY_NAME;
+		// dozens
+		if (ten > 1) {
+			numberInFull += Money.DOZENS[ten];
+			if ( unit > 0) {
+				numberInFull += Money.CONECTOR;
+			}
+		}
+		
+		// teens
+		if (ten == 1) {
+			numberInFull += Money.TEENS[unit];
+		}
+		
+		// units
+		if (unit > 0 && ten != 1) {
+			numberInFull += Money.UNITS[unit];
+		}
+
+		numberInFull += Money.CURRENCY_NAME;
 		numberInFull = numberInFull.substring(0, 1).toUpperCase() + numberInFull.substring(1);
-		
+
 		return numberInFull;
 	}
 }
--- src/test/java/asd/devops/aula03/MoneyTest.java
+++ src/test/java/asd/devops/aula03/MoneyTest.java
@@ -13,22 +13,70 @@ public class MoneyTest extends TestCase {
 	public static Test suite() {
 		return new TestSuite(MoneyTest.class);
 	}
-	
+
 	public void testShouldReturnOitoReaisWhen8() {
 		Money money = new Money(8.00);
-		
+
 		assertEquals("Oito reais", money.toNumberInFull());
+	}
+	
+	public void testShouldReturnDezReaisWhen10() {
+		Money money = new Money(10.00);
+
+		assertEquals("Dez reais", money.toNumberInFull());
+	}
+
+	public void testShouldReturnQuinzeReaisWhen15() {
+		Money money = new Money(15.00);
+
+		assertEquals("Quinze reais", money.toNumberInFull());
+	}
+
+	public void testShouldReturnVinteReaisWhen20() {
+		Money money = new Money(20.00);
+
+		assertEquals("Vinte reais", money.toNumberInFull());
 	}
 
 	public void testShouldReturnQuarentaEDoisReaisWhen42() {
 		Money money = new Money(42.00);
-		
+
 		assertEquals("Quarenta e dois reais", money.toNumberInFull());
 	}
+
+	public void testShouldReturnCemReaisWhen100() {
+		Money money = new Money(100.00);
+
+		assertEquals("Cem reais", money.toNumberInFull());
+	}
+
+	public void testShouldReturnCentoETresReaisWhen103() {
+		Money money = new Money(103.00);
+
+		assertEquals("Cento e três reais", money.toNumberInFull());
+	}
+
+	public void testShouldReturnDuzentosETrintaReaisWhen230() {
+		Money money = new Money(230.00);
+
+		assertEquals("Duzentos e trinta reais", money.toNumberInFull());
+	}
 	
+	public void testShouldReturnTrezentosEDezesseisReaisWhen216() {
+		Money money = new Money(216.00);
+
+		assertEquals("Duzentos e dezesseis reais", money.toNumberInFull());
+	}
+
 	public void testShouldReturnSeiscentosESetentaEOitoReaisWhen678() {
 		Money money = new Money(678.00);
-		
+
 		assertEquals("Seiscentos e setenta e oito reais", money.toNumberInFull());
+	}
+
+	public void testShouldReturnNovecentosReaisWhen900() {
+		Money money = new Money(900.00);
+
+		assertEquals("Novecentos reais", money.toNumberInFull());
 	}
 }
